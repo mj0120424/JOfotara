091ca0b2a59d3605df32971475535ed01919d567
BaselWaheed <baselwaheed66@gmail.com>
2025-04-02 15:57:04 +0000

    fix sales invoice js for button

--- jo_fotara/public/js/sales_invoice.js
+++ jo_fotara/public/js/sales_invoice.js
@@ -7,7 +7,7 @@ frappe.ui.form.on("Sales Invoice" , {
 
         let AllowedCompany = frappe.boot.jofotara_settings ;
 
-        // if (frm.doc.docstatus == 1 && frm.doc.uploaded_to_jofotara == 0 && AllowedCompany.includes(frm.doc.company)) {
+        if (frm.doc.docstatus == 1 && frm.doc.uploaded_to_jofotara == 0 && AllowedCompany.includes(frm.doc.company)) {
             frm.add_custom_button(__("Resend To Jofotara") , () => {
                 frappe.call({
                     method: "jo_fotara.fotara.invoice.resend_invoice",
@@ -21,7 +21,7 @@ frappe.ui.form.on("Sales Invoice" , {
                     }
                 })
             })      
-        // }
+        }
     }
 
 })
